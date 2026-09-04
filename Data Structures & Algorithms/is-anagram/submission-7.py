cecd7f9974d37ed92df5ce4d410c3c44b808cbd3
Rishit Mohan <135713256+Rish1406@users.noreply.github.com>
2026-09-04 17:32:07 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_list=list(s)
+        t_list=list(t)
+        # print(s_list,t_list)
+        if(Counter(s_list)==Counter(t_list)):
+            return True
+        else:
+            return False
