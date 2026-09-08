1ccff2e9d5fa68ea1ebb58a061a7f3962f679f03
Rishit Mohan <135713256+Rish1406@users.noreply.github.com>
2026-09-08 13:46:33 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.py
@@ -0,0 +1,18 @@
+#We use 2 pointers at extreme left and extreme right as we want to maximise the width. we shift based on the one with the max height as we want the maximum area off container
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_area=0
+        i=0
+        j=len(heights)-1
+        while i<j:
+            area=(j-i)*min(heights[i],heights[j])
+            max_area=max(max_area,area)
+            if(heights[i]>=heights[j]):
+                j-=1
+            elif(heights[i]<heights[j]):
+                i+=1
+        return max_area
+
+        
+        
