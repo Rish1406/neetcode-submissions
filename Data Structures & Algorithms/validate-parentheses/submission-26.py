50a57882d4986ef0d554abc6acd7999cebdef896
Rishit Mohan <135713256+Rish1406@users.noreply.github.com>
2026-09-10 13:44:19 +0530

    Add: validate-parentheses - submission-26

---
+++ Data Structures & Algorithms/validate-parentheses/submission-26.py
@@ -0,0 +1,22 @@
+#uses ascii
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        i=0
+        while i<len(s):
+            if not stack:
+                stack.append(s[i])
+            else:
+                current=ord(s[i])
+                topofstack=ord(stack[-1])
+                diff=current-topofstack
+                if diff==1 or diff==2:
+                    stack.pop()
+                else:
+                    stack.append(s[i])
+            i+=1
+        if not stack:
+            return True
+        else:
+            return False
+
