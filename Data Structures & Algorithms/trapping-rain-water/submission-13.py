7ff0c6e82f4d81f49ec0b4a35f37ef1019bc6048
Rishit Mohan <135713256+Rish1406@users.noreply.github.com>
2026-09-10 12:46:30 +0530

    Add: trapping-rain-water - submission-13

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-13.py
@@ -0,0 +1,19 @@
+#Solutiom 1 is more easier way of understanding, can be further optimised to this code
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l,r=0,len(height)-1#defining the left and right pointer
+        maxLeft,maxRight=height[l],height[r]
+        res=0
+        while l<r:
+            if(maxLeft<maxRight):#that is if left boundary is minimum
+                l+=1
+                maxLeft=max(maxLeft,height[l])
+                res+=maxLeft-height[l]#Will never be 0
+            else:#if right boundary is minimum or both are equal(if maxLeft and maxRight are equal, it does not matter if you shift l or r)
+                r-=1
+                temp=maxRight-height[r]
+                if(temp>-1):#If positive
+                    res+=temp
+                maxRight=max(maxRight,height[r])
+        return res
+        
