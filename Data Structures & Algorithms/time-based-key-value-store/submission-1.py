de0fea47dcb5f8b459a625f793621663cf5f2eeb
Rishit Mohan <135713256+Rish1406@users.noreply.github.com>
2026-09-18 12:52:57 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class TimeMap:
+
+    def __init__(self):
+        self.store=defaultdict(list)#key:list of [value,timestamp]
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append([value,timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        values=self.store[key]
+        l,r=0,len(values)-1
+        res=""
+        #binary search of the values as they are always in ascending
+        while l<=r:
+            mid=(l+r)//2
+            if(values[mid][1]<=timestamp):
+                res=values[mid][0]#saving the value
+                l=mid+1#need to check if there exist a higher value which is still lesser than timestamp
+            else:
+                r=mid-1
+        return res
+
+        
